1dc6a3431b773aa62b03ab5af59a7bb49873e39f
yun45 <numb3r@126.com>
2017-05-11 08:31:30 +0800

    1.1

---
+++ app/src/main/java/com/zhixingweather/zhixingweather/db/City.java
@@ -0,0 +1,45 @@
+package com.zhixingweather.zhixingweather.db;
+
+/**
+ * Created by Administrator on 2017-05-04.
+ */
+
+public class City extends Datasupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/zhixingweather/zhixingweather/db/County.java
@@ -0,0 +1,45 @@
+package com.zhixingweather.zhixingweather.db;
+
+/**
+ * Created by Administrator on 2017-05-04.
+ */
+
+public class County extends Datasupport {
+
+    private int id;
+    private String countyName;
+    private int cityId;
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
